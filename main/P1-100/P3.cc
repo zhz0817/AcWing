a02ff23856a1933e6560da0ca65ab40a8b696cd9
zhz <zhz15114585761@gmail.com>
2024-06-03 22:54:48 +0800

    完全背包

--- main/P1-100/P3.cc
+++ main/P1-100/P3.cc
@@ -1,30 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	int n,v;
-    cin>>n>>v;
-    vector<int> weights(n,0);
-    vector<int> values(n,0);
-    for(int i=0;i<n;i++){
-        cin>>weights[i]>>values[i];
+#define FOR_INC(i, start, end) for (int i = start; i < end; ++i)
+#define FOR_DEC(i, start, end) for (int i = start; i > end; --i)
+#define FOR_INC_EQUAL(i, start, end) for (int i = start; i <= end; ++i)
+#define FOR_DEC_EQUAL(i, start, end) for (int i = start; i >= end; --i)
+
+int main() {
+  int n, v;
+  cin >> n >> v;
+  vector<int> weights(n + 1, 0);
+  vector<int> values(n + 1, 0);
+  FOR_INC_EQUAL(i, 1, n) { cin >> weights[i] >> values[i]; }
+  int dp[v + 1];
+  memset(dp, 0, sizeof(dp));
+  FOR_INC_EQUAL(i, 1, n) {
+    FOR_INC_EQUAL(j, weights[i], v) {
+      dp[j] = std::max(dp[j], dp[j - weights[i]] + values[i]);
     }
-    int dp[n+1][v+1];
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=v;j++){
-            dp[i][j] = 0;
-        }
-    }
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=v;j++){
-            if(j<weights[i-1]){
-                dp[i][j] = dp[i-1][j];
-            }else{
-                dp[i][j] = std::max(dp[i-1][j],dp[i][j-weights[i-1]]+values[i-1]);
-            }
-        }
-    }
-    cout<<dp[n][v]<<endl;
-	return 0;
+  }
+  cout << dp[v] << endl;
+  return 0;
 }

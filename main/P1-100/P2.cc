6d1aea29afe5e4665ea82c5be407babf716e264c
zhz <zhz15114585761@gmail.com>
2024-06-03 22:03:49 +0800

    01背包

--- main/P1-100/P2.cc
+++ main/P1-100/P2.cc
@@ -1,30 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	int n,v;
-    cin>>n>>v;
-    vector<int> weights(n,0);
-    vector<int> values(n,0);
-    for(int i=0;i<n;i++){
-        cin>>weights[i]>>values[i];
-    }
-    int dp[n+1][v+1];
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=v;j++){
-            dp[i][j] = 0;
-        }
-    }
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=v;j++){
-            if(j<weights[i-1]){
-                dp[i][j] = dp[i-1][j];
-            }else{
-                dp[i][j] = std::max(dp[i-1][j],dp[i-1][j-weights[i-1]]+values[i-1]);
-            }
-        }
-    }
-    cout<<dp[n][v]<<endl;
-	return 0;
+#define FOR_INC(i, start, end) for (int i = start; i < end; ++i)
+#define FOR_DEC(i, start, end) for (int i = start; i > end; --i)
+#define FOR_INC_EQUAL(i, start, end) for (int i = start; i <= end; ++i)
+#define FOR_DEC_EQUAL(i, start, end) for (int i = start; i >= end; --i)
+
+int main() {
+  int n, v;
+  cin >> n >> v;
+  int dp[v + 1];
+  memset(dp, 0, sizeof(dp));
+  FOR_INC_EQUAL(i, 1, n) {
+    int weight, value;
+    cin >> weight >> value;
+    FOR_DEC_EQUAL(j, v, weight) { dp[j] = max(dp[j], dp[j - weight] + value); }
+  }
+  cout << dp[v] << endl;
+  return 0;
 }
+
+// https://www.acwing.com/problem/content/discussion/content/2807/
